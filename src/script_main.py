a9df7cca7489dcfd1d3d0c96e9f2a79c91ee2afa
Victor Bartholomeu <bartholomeuvictor@gmail.com>
2025-05-25 18:58:41 -0300

    feat(GUI): finished GUI implementation. Ready to be merged

--- src/script_main.py
+++ src/script_main.py
@@ -7,10 +7,13 @@ from service_manager import ServiceManager
 from datetime import date
 import logging
 import cowsay
+from visual_interface import Visuals
+import threading
+
 
 #Logging methods
 def print_and_log(level_name, message, logger=logging):
-        print(message)
+        gui.append_string_text_box(message)
         match level_name:
             case 'INFO':
                 logger.info(message)
@@ -24,39 +27,72 @@ def add_file_to_review_list(review_list_path, review_file_name, message=''):
             else:
                 file.write(f'Arquivo: {review_file_name}\n')
 
-def main():
-    print('Projeto Banco de Dados \n')
-    #Define necessary files, such as directory that contains PDFs, credentials json path and token json path (which,
-    # by default, will be name 'my_token.json' inside the same directory as credentials json)
-    standard_config = input('Inserir informações na planilha padrão (S/N)?')
-    while not (standard_config.lower() == 's' or standard_config.lower() == 'n'):
-        standard_config = input('Insira uma resposta válida para uso de planilha padrão (S/N): ')
 
-    match standard_config.lower():
-        case 'n':
-            __spreadsheet_id = input('Digite o id da planilha (encontrado na URL): ')
-            sheet_name = input('Digite o nome da aba da planilha: ')
-            cells_range = input('Digite o range em notação A1: ')
-        case 's' | _:
-            __spreadsheet_id = '1wDG00ttRcT57VVPVppvAxwNvImeSJ_XVi-f5dJDmnsE' #default spreadsheet id for the project
-            sheet_name = input('Digite o nome da aba da planilha: ')
-            cells_range = sheet_name  # since we're using only append function, no need to define A1 notation range
+#defining button click actions
+def pdf_btn_action():
+    dir_path = gui.select_dir("Selecione uma pasta")
+    if dir_path != "":
+        dir_path = os.path.normpath(dir_path)
+        if not (os.path.exists(dir_path) and os.path.isdir(dir_path)): 
+            gui.show_dialog_message("Selecione uma pasta válida!", "error")
+            gui.reset_pdf_dir_selection()
+        else:
+            gui.update_pdf_dir_label(dir_path)
+    else:
+        gui.reset_pdf_dir_selection()
 
-    directory_path = input('Digite o caminho da pasta onde estão os PDFS e aperte Enter: ')
-    directory_path = os.path.normpath(directory_path)
-    while not (os.path.exists(directory_path) and os.path.isdir(directory_path)):
-        directory_path = input('Digite um caminho de pasta válido: ')
-        directory_path = os.path.normpath(directory_path)
+def credential_btn_action():
+    credential_path = gui.select_file("Selecione o arquivo .JSON de credencial")
+    if credential_path != "":
+        credential_path = os.path.normpath(credential_path)
+        if not (os.path.exists(credential_path) and credential_path.split('.')[-1].lower() == 'json'):
+            gui.show_dialog_message("Selecione um arquivo de credencial válido!", "error")
+            gui.reset_credential_file_selection()
+        else:
+            gui.update_credential_file_label(credential_path)
+    else:
+        gui.reset_credential_file_selection()
 
-    credentials_path = input('Digite o caminho do arquivo de credenciais do Google: ')
-    credentials_path = os.path.normpath(credentials_path)
+def check_filled_fields() -> bool:
+    if gui.get_selected_pdf_dir_string() == gui.get_initial_selected_pdf_dir_string():
+        gui.show_dialog_message("Pasta de PDFs ainda não foi selecionada! Selecione uma pasta válida antes de continuar.", 
+                                "warning")
+        return False
 
-    while not (os.path.exists(credentials_path) and credentials_path.split('.')[-1].lower() == 'json'):
-        credentials_path = input('Digite um caminho de arquivo de credenciais válido: ')
-        credentials_path = os.path.normpath(credentials_path)
+    if gui.get_selected_credential_file_string() == gui.get_initial_selected_credential_file_string():
+        gui.show_dialog_message("Arquivo de credencial ainda não foi selecionado! Selecione um aquivo de credendial "+
+            "válido antes de continuar.", "warning")
+        return False
 
+    if gui.get_spreadsheet_id().strip() == "":
+        gui.show_dialog_message("Id da planilha não foi preenchido! Preencha antes de continuar!", "warning")
+        return False
+    
+    if gui.get_sheet_name().strip() == "":
+        gui.show_dialog_message("O nome da aba da planilha não foi preenchido!  \n Preencha antes de continuar.", "warning")
+        return False
+
+    return True
+
+
+def start_btn_action():
+    gui.capture_typed_spreadsheet_id_manual()
+    gui.capture_typed_sheet_name_manual()
+    if check_filled_fields():
+        gui.toggle_text_box_visibility()
+        #call function to start script
+        gui.make_all_widgets_unclickable()
+        threading.Thread(target=execute_script, daemon=True).start()
+        #execute_script()
+
+def execute_script():
+    directory_path = gui.get_selected_pdf_dir_string()
+    credentials_path = gui.get_selected_credential_file_string()
     credentials_path_dir = os.path.dirname(credentials_path)
     token_path = os.path.join(credentials_path_dir, 'my_token.json')
+    spreadsheet_id = gui.get_spreadsheet_id()
+    sheet_name = gui.get_sheet_name()
+    cells_range = sheet_name
 
     #Logging setup
     today_date = date.today().strftime('%d.%m.%Y')
@@ -76,6 +112,8 @@ def main():
     
 
     print_and_log('INFO', 'Iniciando execução do script:', log)
+
+
     try:
         # Authenticate and start service from Google API--------------------------
         service_manager = ServiceManager(credentials_path, token_path)
@@ -99,26 +137,29 @@ def main():
             text_content = pdf_reader_module.save_content_into_string()
 
             #Extract information fom PDF----------------------------------------------
-            info_extractor = BiopsyInfoExtractor.from_model(text_content, 'model_1')
+            info_extractor = BiopsyInfoExtractor.from_model(text_content, 'model_1', gui.append_string_text_box)
             if info_extractor.validate_biopsy():
-                print_and_log('INFO',f'Lendo arquivo: {file_name}', log)
+                print_and_log('INFO',f'-Lendo arquivo: {file_name}', log)
                 values_to_be_appended = info_extractor.organize_information_into_sheets_api_input_format()
-                biopsy_order_number = info_extractor.get_order_number() #usded for document identification
+                biopsy_order_number = info_extractor.get_order_number() #used for document identification
+
                 try:
                     #Insert data into sheet--------------------------------------------------
-                    sheets_manipulator = SheetsManipulator(service_manager, __spreadsheet_id)
+                    sheets_manipulator = SheetsManipulator(service_manager, spreadsheet_id)
                     sheets_manipulator.copy_table_banding_to_new_row(sheet_name)
-                    sheets_manipulator.append_new_row(__spreadsheet_id, cells_range, [values_to_be_appended])
+                    sheets_manipulator.append_new_row(spreadsheet_id, cells_range, [values_to_be_appended])
                     print_and_log('INFO',f'Informações do arquivo {file_name} (pedido: {biopsy_order_number}) '
-                                        f'incluídas na planilha com sucesso', log)
+                                    f'incluídas na planilha com sucesso', log)
+
                 except Exception as e:
-                    print(f'Erro ao inserir informações do arquivo na planilha de Google Sheets. '
-                        f'O arquivo {file_name} deverá ser inserido manualmente. \n Detalhes do erro:\n {e}')
+                    gui.append_string_text_box(f'Erro ao inserir informações do arquivo na planilha de Google Sheets. '
+                    f'O arquivo {file_name} deverá ser inserido manualmente. \n Detalhes do erro:\n {e}')
                     traceback_register = traceback.format_exc()
                     log.error(f'Erro ao inserir informações do arquivo {file_name} na planilha de Google Sheets. Detalhes do '
-                            f'erro:\n {e} \n\n Traceback para consulta:\n{traceback_register}\n\n')
+                        f'erro:\n {e} \n\n Traceback para consulta:\n{traceback_register}\n\n')
                     add_file_to_review_list(review_list_path, file_name, 'Realizar inserção manual de dados na planilha')
                     continue
+
 
                 try:
                     #Finish renaming file in directory---------------------------------------
@@ -131,13 +172,13 @@ def main():
                         while os.path.exists(alternative_file_path):
                             n+=1
                             alternative_file_path = os.path.join(directory_path, f'{biopsy_order_number}_{n}.pdf')
-                        print(f'biópsia de pedido nº {biopsy_order_number} foi lida repetidamente')
+                        gui.append_string_text_box(f'biópsia de pedido nº {biopsy_order_number} foi lida repetidamente')
                         new_file_path = alternative_file_path
                     os.rename(old_file_path, new_file_path)
                     log.info(f'Arquivo {file_name} (pedido: {biopsy_order_number}) renomeado com sucesso')
                     log.info(f'OK: arquivo {file_name} (pedido: {biopsy_order_number}).\n')
                 except Exception as e:
-                    print(f'Erro ao renomear o arquivo na pasta {directory_path}. O arquivo {file_name} deverá'
+                    gui.append_string_text_box(f'Erro ao renomear o arquivo na pasta {directory_path}. O arquivo {file_name} deverá'
                         f'ser renomeado manualmente. \n Detalhes do erro:\n{e}')
                     traceback_register = traceback.format_exc()
                     log.error(
@@ -146,13 +187,16 @@ def main():
                     add_file_to_review_list(review_list_path, file_name, 'Iserção ok. Renomear arquivo manualmente')
                     continue
             else:
-                print(f'Documento {file_name} não é uma biópsia válida, e, portanto, será pulado.'
+                gui.append_string_text_box(f'Documento {file_name} não é uma biópsia válida, e, portanto, será pulado.'
                     f'\n Faça verificação manual')
 
-    print(cowsay.get_output_string('cow', 'Fim do script!'))
+    gui.append_string_text_box(cowsay.get_output_string('cow', 'Fim do script!'))
 
-if __name__ == '__main__':
+        
+def main():
+    global gui
+    gui = Visuals(pdf_btn_action, credential_btn_action, start_btn_action)
+    gui.start_main_loop()
+
+if __name__ == "__main__":
     main()
-
-
-
